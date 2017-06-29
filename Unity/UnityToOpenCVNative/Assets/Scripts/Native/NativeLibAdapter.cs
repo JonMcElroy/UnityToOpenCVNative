8082d3fc32728c653a46841c1b56a4791b6865c7
Jon <mcelroy.jon@gmail.com>
2017-06-29 10:39:41 -0400

    Initial push of sample Android project, associated C++ files, sample Unity project and C# files.

---
+++ Unity/UnityToOpenCVNative/Assets/Scripts/Native/NativeLibAdapter.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+using UnityEngine;
+
+/*
+	NativeLibAdapter is an example communication layer between managed C# and native C++
+ */
+public class NativeLibAdapter
+{
+	[DllImport("native-lib")]
+	private static extern int InitCV_Internal(int width, int height);
+
+	[DllImport("native-lib")]
+	private static extern IntPtr SubmitFrame_Internal(int width, int height, IntPtr bufferAddr);
+
+	[DllImport("native-lib")]
+	private static extern int FooTestFunction_Internal();
+
+	public static int InitCV(int width, int height)
+	{
+		int result = InitCV_Internal(width, height);
+		Debug.LogWarning("[NativeLibAdapter] InitCV " + (result == 0 ? "No Errors" : "Error Code : " + result));
+		
+		return result;
+	}
+
+	public static IntPtr SubmitFrame(int width, int height, IntPtr bufferAddr)
+	{
+		IntPtr ret = SubmitFrame_Internal(width, height, bufferAddr);
+		return ret;
+	}
+
+	public static int FooTest()
+	{
+		return FooTestFunction_Internal();
+	}
+
+}
